8acfd82b0119f99257878b511f740f8e484375cf
user160244980349 <mkuznetsov7991@gmail.com>
2021-05-25 14:42:27 +0300

    For screenshots

--- views/annotation.php
+++ views/annotation.php
@@ -3,7 +3,7 @@
 
 <div class="row">
 
-    <side class="col col-md-4 col-lg-3 ">
+    <side class="col col-md-5 col-lg-4 ">
         <div class="card sticky-sidebar">
             <div class="card-header d-flex justify-content-between">
                 <div>Annotation Layers</div>
@@ -15,7 +15,7 @@
         </div>
     </side>
 
-    <main class="col col-md-6 col-lg-6" role="main">
+    <main class="col" role="main">
         <div class="card">
             <div class="card-body">
                 <div id="annotation_surface"><?php echo $text ?></div>
