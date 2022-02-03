7231fc44ec6280188d75b0ba5db5bd3c5c21fc3e
user160244980349 <mkuznetsov7991@gmail.com>
2022-02-03 01:35:45 +0000

    Some

--- example.env.php
+++ example.env.php
@@ -4,15 +4,16 @@ use Engine\Config;
 
 Config::set('env', [
 
-    # Views path
-    'views' => __DIR__ . '/views',
-
     # Debug prints
     'debug' => true,
 
+    # Views path
+    'views'       => __DIR__ . '/views',
+    'resources'   => __DIR__ . '/resources',
+
     # Database credentials
     'db_driver'   => 'mysql',
-    'db_address'  => 'localhost',
+    'db_address'  => 'db',
     'db_name'     => 'iot_annotation',
     'db_user'     => 'iot_annotation',
     'db_password' => 'secret',
